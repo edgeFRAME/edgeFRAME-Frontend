2287b8424e2be923b9f7b98ec24f52566121f6e6
Juan Rodríguez Suárez <alu0101477596@ull.edu.es>
2025-10-06 18:41:20 +0100

    fix: Delete edgeFRAME directory

--- edgeFRAME/src/main.ts
+++
@@ -1,9 +0,0 @@
-import './style.css'
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div class="container">
-    <label for="input-file">
-      <input type="file" accept="image/*" id="input-file" />
-    </label>
-  </div>
-`

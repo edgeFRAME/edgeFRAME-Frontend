cc29cec2cf3eb19a9f6bb2e6d6f6ccb7a74ea9d5
Diego Prieto González <91544254+DiegoPrietoG@users.noreply.github.com>
2025-10-15 14:20:07 +0100

    Snackbars and alerts notifications

--- src/DropFile.tsx
+++ src/DropFile.tsx
@@ -1,11 +1,18 @@
 import './DropFile.css'
 //import cloudUploadImage from './assets/cloud_upload.png';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { Alert, Snackbar } from '@mui/material';
 import { processFile } from './utils/processFile'
 import { error } from './utils/error'
 import { getThumbnail } from './utils/getThumbnail';
 
 import React, { useState } from 'react'
+
+interface SnackbarState {
+    open: boolean;
+    message: string;
+    severity: 'error' | 'success' | 'warning' | 'info';
+}
 
 interface DropFileProps {
     maxSize: number;
@@ -21,12 +28,24 @@ function DropFile({ maxSize, dataUnit = 'MB' }: DropFileProps) {
     const [changeDropArea, setDropArea] = useState(false);
     const [thumbnail, setThumbnail] = useState("");
     const [file, setFile] = useState<File | null>(null);
+    const [snackbar, setSnackbar] = useState<SnackbarState>({
+        open: false,
+        message: '',
+        severity: 'info',
+    });
+
     const handleDrop = async (e: FileList) => {
         const file_ = e?.[0];
         // if file is null is false
         if (file_) {
             const processedFile = processFile(file_, maxSize, dataUnit);
-            if (processedFile) {
+            if (typeof processedFile === 'string') {
+                setSnackbar({
+                    open: true,
+                    message: processedFile,
+                    severity: 'error',
+                });
+            } else {
                 console.log('File processed successfully:');
                 setThumbnail(await getThumbnail(file_));
                 setDropArea(true); // re-render
@@ -35,51 +54,88 @@ function DropFile({ maxSize, dataUnit = 'MB' }: DropFileProps) {
             }
         }
         // error: problem with the file, try again
-        else error(2);
+        else {
+            setSnackbar({
+                open: true,
+                message: error(2),
+                severity: 'error',
+            });
+        }
 
         // Cambio en el estilo del drop-area
     }
     if (changeDropArea) {
         return <>
-            <label htmlFor="input-file" id="drop-area" onDrop={(e : React.DragEvent<HTMLLabelElement>) => {
-                                                                                                            e.preventDefault();
-                                                                                                            e.stopPropagation();
-                                                                                                            handleDrop(e.dataTransfer.files);
-                                                                                                            }}
-                                                                                                            onDragOver={handleDragOver}>
-                <input id="input-file" type="file" accept="video/*" hidden onChange={(e : React.ChangeEvent<HTMLInputElement>) => {
-                                                                                                                                    e.preventDefault();
-                                                                                                                                    e.stopPropagation();
-                                                                                                                                    handleDrop(e.target.files!)}}/>
-                <div id="img-view" className='success'>
-                    <div id="img-thumb">
-                        <img id="thumbnail" alt="video thumbnail" src={thumbnail} />
+            <div id="dropfile-container">
+                <label htmlFor="input-file" id="drop-area" onDrop={(e: React.DragEvent<HTMLLabelElement>) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    handleDrop(e.dataTransfer.files);
+                }}
+                    onDragOver={handleDragOver}>
+                    <input id="input-file" type="file" accept="video/*" hidden onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        handleDrop(e.target.files!)
+                    }} />
+                    <div id="img-view" className='success'>
+                        <div id="img-thumb">
+                            <img id="thumbnail" alt="video thumbnail" src={thumbnail} />
+                        </div>
+                        <div id="file-info">
+                            <p><strong>Name:</strong> {file?.name}</p>
+                            <p><strong>Size:</strong> {file?.size} bytes</p>
+                            <p><strong>Type:</strong> {file?.type}</p>
+                        </div>
                     </div>
-                    <div id="file-info">
-                        <p><strong>Name:</strong> {file?.name}</p>
-                        <p><strong>Size:</strong> {file?.size} bytes</p>
-                        <p><strong>Type:</strong> {file?.type}</p>
-                    </div>
+                </label>
+                <div id='notification-order'>
+                    <Snackbar
+                        open={snackbar.open}
+                        autoHideDuration={6000}
+                        onClose={() => setSnackbar({ ...snackbar, open: false})}
+                        sx={{
+                            position: 'relative',
+                        }}
+                    >
+                        <Alert severity={snackbar.severity} sx={{ width: '100%' }}>{snackbar.message}</Alert>
+                    </Snackbar>
                 </div>
-            </label>
+            </div >
         </>
     }
     return <>
-        <label htmlFor="input-file" id="drop-area" onDrop={(e : React.DragEvent<HTMLLabelElement>) => {
-                                                                                                        e.preventDefault();
-                                                                                                        e.stopPropagation();
-                                                                                                        handleDrop(e.dataTransfer.files)}}
-                                                                                                        onDragOver={handleDragOver}>
-            <input id="input-file" type="file" accept="video/*" hidden onChange={(e : React.ChangeEvent<HTMLInputElement>) => {
-                                                                                                                                    e.preventDefault();
-                                                                                                                                    e.stopPropagation();
-                                                                                                                                    handleDrop(e.target.files!)}}/>
-            <div id="img-view">
-                <CloudUploadIcon sx={{ fontSize: 150, color: 'white' }} />
-                <p>Drag and drop or click here to upload image</p>
-                <span>Max size {maxSize} {dataUnit}</span>
+        <div id="dropfile-container">
+            <label htmlFor="input-file" id="drop-area" onDrop={(e: React.DragEvent<HTMLLabelElement>) => {
+                e.preventDefault();
+                e.stopPropagation();
+                handleDrop(e.dataTransfer.files)
+            }}
+                onDragOver={handleDragOver}>
+                <input id="input-file" type="file" accept="video/*" hidden onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    handleDrop(e.target.files!)
+                }} />
+                <div id="img-view">
+                    <CloudUploadIcon sx={{ fontSize: 150, color: 'white' }} />
+                    <p>Drag and drop or click here to upload image</p>
+                    <span>Max size {maxSize} {dataUnit}</span>
+                </div>
+            </label>
+            <div id='notification-order'>
+                <Snackbar
+                    open={snackbar.open}
+                    autoHideDuration={10000}
+                    onClose={() => setSnackbar({ ...snackbar, open: false})}
+                    sx={{
+                        position: 'relative',
+                    }}
+                >
+                    <Alert severity={snackbar.severity} sx={{ width: '100%' }}>{snackbar.message}</Alert>
+                </Snackbar>
             </div>
-        </label>
+        </div>
     </>;
 }
 
--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,23 +1,18 @@
-export function error(errorCode : number) {
+export function error(errorCode: number) {
   switch (errorCode) {
     case 1:
-      alert('Error: File is not an mp4 video');
-      console.error('Error: File is not an mp4 video');
+      return ('Error: File is not an mp4 video');
       break;
     case 2:
-      alert('Error: Problem with the file, please try again');
-      console.error('Error: Problem with the file, please try again');
+      return ('Error: Problem with the file, please try again');
       break;
     case 3:
-      alert('Error: File is too big');
-      console.error('Error: File is too big');
+      return ('Error: File is too big');
       break;
     case 4:
-      alert('Error: File is empty');
-      console.error('Error: File is empty');
+      return('Error: File is empty');
       break;
     default:
-      alert('Error: Unknown error');
-      console.error('Error: Unknown error');
+      return ('Error: Unknown error');
   }
 }
